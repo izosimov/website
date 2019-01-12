f77c12016166498272df8c84138cf4eed9e5e10a
Артём Изосимов <1996loko@rambler.ru>
2019-01-12 15:01:38 +0300

    for release

--- src/scripts/scripts.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    const siteSpinnerWrap = document.getElementsByClassName('site-spinner-wrap')[0];
-
-    window.addEventListener('load', (e) => {
-        siteSpinnerWrap.style.display = 'none';
-    });
-}());
